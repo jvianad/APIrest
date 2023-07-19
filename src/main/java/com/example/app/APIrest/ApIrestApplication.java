94464a921ea713fa762f3654739a8d526789a4fe
jvianad <jeanvianademares@gmail.com>
2023-07-18 19:51:40 -0500

    Creando una api rest crud desde cero

---
+++ src/main/java/com/example/app/APIrest/ApIrestApplication.java
@@ -0,0 +1,13 @@
+package com.example.app.APIrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/app/APIrest/ApIrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app.APIrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
