825617eae209cdbd3fa4aa69ab07dce4b7e86b8f
jvianad <jeanvianademares@gmail.com>
2023-07-18 20:37:30 -0500

    Creamos metodo HTTP GET en rama Jean

--- src/main/java/com/example/app/APIrest/Controller/TaskController.java
+++ src/main/java/com/example/app/APIrest/Controller/TaskController.java
@@ -1,12 +1,22 @@
 package com.example.app.APIrest.Controller;
+import com.example.app.APIrest.Model.Task;
+import com.example.app.APIrest.Repository.iToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TaskController {
-    @GetMapping(value = "/")
-    public String holamundo(){
-        return "Hola Mundo!!!";
+    @Autowired
+    private iToDoRepository todorepository;
+
+    @GetMapping(value = "/tasks")
+    public List<Task> getTask(){
+        return todorepository.findAll();
     }
     
+
+
 }
